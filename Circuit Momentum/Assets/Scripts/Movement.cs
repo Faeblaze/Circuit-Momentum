7555ca7de96c50b3b993e895b92fd7232b9649ca
Faeblaze <mjbstankovic27@gmail.com>
2018-12-05 12:58:43 +1100

    initial rotation, movement

---
+++ Circuit Momentum/Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float acceleration = 8;
+    [SerializeField] float turnSpeed = 5;
+
+    Quaternion targetRotation;
+    Rigidbody _rigidbody;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SetRotationPoint();
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+	}
+
+    private void SetRotationPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if(plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+        }
+}
+    private void FixedUpdate()
+    {
+        float accelerationInput = acceleration * (Input.GetMouseButton(0) ? 1 : Input.GetMouseButton(1) ? -1 : 0) * Time.fixedDeltaTime;
+        _rigidbody.AddRelativeForce(Vector3.forward * accelerationInput);
+    }
+}
---
+++ Circuit Momentum/Assets/Scripts/TopDownCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField] Transform observeable;
+    [SerializeField] float aheadSpeed;
+    [SerializeField] float followDamping;
+    [SerializeField] float cameraHeight;
+
+    Rigidbody _observableRigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _observableRigidBody = observeable.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (observeable == null)
+            return;
+
+        Vector3 targetPosition = observeable.position + Vector3.up * cameraHeight + _observableRigidBody.velocity * aheadSpeed;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followDamping * Time.deltaTime);
+	}
+
+  //  https://www.youtube.com/watch?v=pAsCXXsuB1M
+}
