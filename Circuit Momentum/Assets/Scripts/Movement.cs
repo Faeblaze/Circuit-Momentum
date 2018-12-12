108ef1e9d58e55c878efe33fb8c25da6e071e54e
Faeblaze <mjbstankovic27@gmail.com>
2018-12-12 16:08:44 +1100

    NavMesh,Fixed Movement

--- Circuit Momentum/Assets/Scripts/Movement.cs
+++ Circuit Momentum/Assets/Scripts/Movement.cs
@@ -27,14 +27,21 @@ public class Movement : MonoBehaviour
     private void SetRotationPoint()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.up, Vector3.zero);
-        float distance;
-        if(plane.Raycast(ray, out distance))
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo))
         {
-            Vector3 target = ray.GetPoint(distance);
-            Vector3 direction = target - transform.position;
-            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+            Vector3 target = hitInfo.point;
+
+            target.y = transform.position.y;
+
+            Vector3 heading = target - transform.position;
+            Vector3 direction = heading / heading.magnitude;
+
+            targetRotation = Quaternion.LookRotation(direction);
+            //targetRotation = Quaternion.LookRotation
+            //Vector3 direction = target - transform.position;
+            //float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            //targetRotation = Quaternion.Euler(0, rotationAngle, 0);
         }
 }
     private void FixedUpdate()
